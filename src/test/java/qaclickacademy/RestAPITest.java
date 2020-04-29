8c798746e1a172d59dcc02646b963c784a0c0057
Sabir Ali Shaik <skshabirali1@gmail.com>
2020-04-29 18:26:40 +0530

    First commit

---
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+
+	
+	@Test
+	public void postjira() {
+		System.out.println("post jira");
+	}
+	
+	@Test
+	public void deletetwitter() {
+		System.out.println("delete twitter");
+	}
+}
