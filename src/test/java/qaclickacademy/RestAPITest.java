2680acc515023c39b36f8fa03dc146fa07d26431
Sabir Ali Shaik <skshabirali1@gmail.com>
2020-04-30 12:04:44 +0530

    Changes to RESTAPI file

--- src/test/java/qaclickacademy/RestAPITest.java
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -8,10 +8,12 @@ public class RestAPITest {
 	@Test
 	public void postjira() {
 		System.out.println("post jira");
+		System.out.println("post jira2");
 	}
 	
 	@Test
 	public void deletetwitter() {
 		System.out.println("delete twitter");
+		System.out.println("delete twitter2");
 	}
 }
